e1f55b00022dfa57ec95a1d4e0dd2ffa4ea641f1
Carlos Eduardo <ce416669@gmail.com>
2026-03-28 12:17:59 -0300

    Dei um Talendo no repositório dfe prog 2  e add exercícios e aulas

---
+++ 3-semestre/prog-II/exercicios/02/nome.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string nome;
+
+    // Simples
+    cout << "Bem-vindo, Carlos!" << endl;
+    
+    // Composto
+    cout << "Informe seu nome: ";
+    cin >> nome;
+
+    cout << "Bem-vindo, " << nome << "!" << endl;
+
+}
