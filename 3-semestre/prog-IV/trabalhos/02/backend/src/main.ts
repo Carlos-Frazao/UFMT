51e1eec644ae5188a608a45781ee811506c4cf50
Carlos-Frazao <ce416669@gmail.com>
2026-07-09 22:55:41 -0300

    Adicionando o Swagger

--- 3-semestre/prog-IV/trabalhos/02/backend/src/main.ts
+++ 3-semestre/prog-IV/trabalhos/02/backend/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  const config = new DocumentBuilder()
+  .setTitle('Wiki Mr. Robot API')
+  .setDescription('Documentação das rotas da Wiki fSociety')
+  .setVersion('1.0')
+  .addBearerAuth() // Isso permite colar o Token JWT no Swagger
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 
